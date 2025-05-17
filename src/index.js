cdaea838e61919819735e80c8857ed146c4a40ab
VadymVlodarchyk <vadymvlodarchyk@gmail.com>
2025-05-17 17:36:58 +0100

    Fix: correct relative imports inside src

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import { setupServer } from './src/server.js';
-import { initMongoConnection } from './src/db/initMongoConnection.js';
+import { setupServer } from './server.js';
+import { initMongoConnection } from './db/initMongoConnection.js';
+
 
 const startApp = async () => {
   try {
