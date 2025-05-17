d7513afb056de52171365bdf3a90380f1d43bb9d
VadymVlodarchyk <vadymvlodarchyk@gmail.com>
2025-05-17 19:12:46 +0100

    🚿 Remove /test route, ready for final deployment

--- src/server.js
+++ src/server.js
@@ -15,26 +15,15 @@ export const setupServer = () => {
   app.use(pino());
   app.use(express.json());
 
-  // Тестовий маршрут
-  app.get('/test', (req, res) => {
-    res.status(200).json({
-      status: 'ok',
-      message: '🔥 This is /test route. Server is definitely working.',
-    });
-  });
-
-  // Кореневий маршрут
   app.get('/', (req, res) => {
     res.status(200).send('✅ Everything is working perfectly!');
   });
 
-  // Роут для контактів
+
   app.use('/contacts', contactsRouter);
 
-  // Обробка 404
   app.use(notFoundHandler);
 
-  // Обробка помилок
   app.use(errorHandler);
 
   const PORT = process.env.PORT || 3000;
