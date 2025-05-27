0cd16bc78abc347e129b44a82a0687278ac95343
VadymVlodarchyk <vadymvlodarchyk@gmail.com>
2025-05-27 23:00:24 +0100

    ✅ Fix: use correct status code in error handler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,9 +1,12 @@
 export const errorHandler = (err, req, res, next) => {
   console.error('💥 Error:', err.message);
 
-  res.status(500).json({
-    status: 500,
+  const status = err.status || 500;
+  const message = err.message || 'Internal Server Error';
+
+  res.status(status).json({
+    status,
     message: 'Something went wrong',
-    data: err.message,
+    data: message,
   });
 };
