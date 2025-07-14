71782a67aa1fa567feb2b010e93b53e8b73fb0e5
VadymVlodarchyk <vadymvlodarchyk@gmail.com>
2025-07-14 19:42:43 +0100

    Enable photo upload to Cloudinary in POST and PATCH /contacts

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv';
+dotenv.config(); // ✅ ініціалізація .env
 import { setupServer } from './server.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
 
