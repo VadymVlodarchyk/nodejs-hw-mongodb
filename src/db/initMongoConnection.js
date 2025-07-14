361aed64a13d810e06d69aa682548f8c29146f85
VadymVlodarchyk <vadymvlodarchyk@gmail.com>
2025-07-14 18:23:53 +0100

    refactor: remove duplicate contact model from db

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 export const initMongoConnection = async () => {
   try {
-    const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/${process.env.MONGODB_DB}?retryWrites=true&w=majority`;
+    const uri = process.env.MONGODB_URI;
 
     await mongoose.connect(uri);
 
